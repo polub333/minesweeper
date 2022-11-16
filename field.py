55c4862fd11673eef1daa0189b42a1c6b0c5bf26
Ivan Poluboyarinov <polub333@gmail.com>
2022-11-16 16:41:23 +0300

    Creating island under the first click

--- field.py
+++ field.py
@@ -8,18 +8,39 @@ class Field:
         self.y = y
         self.mines_num = mines_num
         self.cells = [[]]
-        self.fill_field()
+        self.digits_pos = []
+        self.init_field()
+        self.place_mines()
 
-    def fill_field(self):
-        mines_pos = list(range(0, self.x*self.y))
-        random.shuffle(mines_pos)
+    def init_field(self):
         for i in range(self.y):
             self.cells.append([])
             for j in range(self.x):
+                self.cells[i].append(cell.Cell(j, i, 0, "digit"))
+                self.digits_pos.append((i, j))
+
+    def place_mines(self):
+        mines_pos = list(range(0, self.x*self.y))
+        random.shuffle(mines_pos)
+        for i in range(self.y):
+            for j in range(self.x):
                 if mines_pos[i*self.y + j] < self.mines_num:
-                    self.cells[i].append(cell.Cell(j, i, 0, "mine"))
-                else:
-                    self.cells[i].append(cell.Cell(j, i, 0, "digit"))
+                    self.cells[i][j] = cell.Cell(j, i, 0, "mine")
+                    self.digits_pos.remove((i, j))
+
+    def create_island(self, x, y):
+        for i in range(y-2 if y > 1 else 0, y+3 if x < self.y-2 else self.y):
+            for j in range(x-2 if x > 1 else 0, x+3 if x<self.x-2 else self.x):
+                if self.cells[i][j].content_type == "mine":
+                    self.cells[i][j] = cell.Cell(j, i, 0, "digit")
+                    new_mine_pos = random.randint(0, len(self.digits_pos)-1)
+                    new_i = self.digits_pos[new_mine_pos][0]
+                    new_j = self.digits_pos[new_mine_pos][1]
+                    self.cells[new_i][new_j] = cell.Cell(new_j, new_i, 0, "mine")
+                    self.digits_pos.remove((new_i, new_j))
+        self.fill_field()
+
+    def fill_field(self):
         for i in range(self.y):
             for j in range(self.x):
                 if self.cells[i][j].content_type == "mine":
@@ -44,7 +65,6 @@ class Field:
                 self.cells[i][j].content.set_number(mines_around)
 
     def open(self, x, y):
-        
         if self.cells[y][x].content_type == "digit" and self.cells[y][x].is_open():
             mines_around = 0
             if y > 0 and self.cells[y-1][x].is_flagged():
--- main.py
+++ main.py
@@ -21,11 +21,13 @@ info_font = pygame.font.SysFont('Arial', 20)
 time_passed = 0
 
 def draw_field(screen, field):
+    opened = 0
     for cell_row in field.cells:
         for cell in cell_row:
             if cell.is_open():
                 if cell.content_type == "mine":
                     pygame.draw.rect(screen, "red", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size))
+                    lose_game()
                 else:
                     if cell.content.number == 0:
                         pygame.draw.rect(screen, "gray", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size))
@@ -34,6 +36,7 @@ def draw_field(screen, field):
                         text = font.render(str(cell.content.number), False, cell.content.color)
                         screen.blit(text, ((cell.x + 0.35)*cell_size, (cell.y + 0.1)*cell_size))
             elif cell.is_flagged():
+                opened += 1
                 pygame.draw.polygon(screen, "red", [((cell.x + 0.2)*cell_size, (cell.y + 0.5)*cell_size),
                                                     ((cell.x + 0.8)*cell_size, (cell.y + 0.2)*cell_size),
                                                     ((cell.x + 0.8)*cell_size, (cell.y + 0.8)*cell_size)])
@@ -41,10 +44,31 @@ def draw_field(screen, field):
 
             else:
                 pygame.draw.rect(screen, "gray", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size), 1)
+    if opened == 40:
+        game_end()
 
 def draw_timer(time_passed):
     text = info_font.render("Time: " + str(time_passed//1000), False, "black")
     screen.blit(text, (WIDTH - 120, 20))
+
+def game_end():
+    global finished
+    finished = True
+    old_record = 0
+    time = time_passed // 1000
+    print("Time: " + time)
+    with open("record.txt", "r") as f:
+        old_record = f.read().rstrip()
+    if(time < int(old_record)):
+        with open("record.txt", "w") as f:
+            f.write(str(time))
+
+def lose_game():
+    global finished
+    finished = True
+    print("You loser")
+
+firstClick = True
 
 while not finished:
     screen.fill("white")
@@ -58,6 +82,9 @@ while not finished:
             finished = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:           # Left mouse button
+                if firstClick:
+                    firstClick = False
+                    field.create_island(event.pos[0] // cell_size, event.pos[1] // cell_size)
                 field.open(event.pos[0] // cell_size, event.pos[1] // cell_size)
             elif event.button == 3:         # Right mouse button
                 field.place_flag(event.pos[0] // cell_size, event.pos[1] // cell_size)
