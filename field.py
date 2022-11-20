265b1f78b04fb87d61523d0828d4ec88b7796add
Ivan Poluboyarinov <polub333@gmail.com>
2022-11-20 21:17:49 +0300

    Added docstrings

--- field.py
+++ field.py
@@ -13,6 +13,10 @@ class Field:
         self.place_mines()
 
     def init_field(self):
+        """
+        Создает поле нужного размера, заполняет клетки нулями.
+        Все клетки помечаются как содержащие цифры.
+        """
         for i in range(self.y):
             self.cells.append([])
             for j in range(self.x):
@@ -20,6 +24,11 @@ class Field:
                 self.digits_pos.append((i, j))
 
     def place_mines(self):
+        """
+        Располагает мины в случайных клетках.
+        Убирает клетку, в которую положена мина, из массива
+        клеток с цифрами.
+        """
         mines_pos = list(range(0, self.x*self.y))
         random.shuffle(mines_pos)
         for i in range(self.y):
@@ -29,8 +38,18 @@ class Field:
                     self.digits_pos.remove((i, j))
 
     def create_island(self, x, y):
+        """
+        Создает 'остров' 5 на 5 без мин.
+        Перемещает все мины, находящиеся в этой области, в 
+        клетки, содержащие цифры.
+        Предварительно, клетки внутри этого квадрата помечаются
+        как не содержащие цифры (чтобы в них опать не попали
+        перемещаемые мины).
+        """
         for i in range(y-2 if y > 1 else 0, y+3 if x < self.y-2 else self.y):
             for j in range(x-2 if x > 1 else 0, x+3 if x<self.x-2 else self.x):
+                if (i, j) in self.digits_pos:
+                    self.digits_pos.remove((i, j))
                 if self.cells[i][j].content_type == "mine":
                     self.cells[i][j] = cell.Cell(j, i, 0, "digit")
                     new_mine_pos = random.randint(0, len(self.digits_pos)-1)
@@ -41,6 +60,9 @@ class Field:
         self.fill_field()
 
     def fill_field(self):
+        """
+        Выставляет цифры на поле, учитывая окружающие мины.
+        """
         for i in range(self.y):
             for j in range(self.x):
                 if self.cells[i][j].content_type == "mine":
@@ -65,6 +87,11 @@ class Field:
                 self.cells[i][j].content.set_number(mines_around)
 
     def open(self, x, y):
+        """
+        Открывает клетку. Если клетка уже открыта, открывает соседние
+        клетки (если вокруг исходной клетки нужное количество влагов).
+        Если клетка пустая - открывает все клетки вокруг нее.
+        """
         if self.cells[y][x].content_type == "digit" and self.cells[y][x].is_open():
             if self.check_all_mines_around_are_open(x, y):
                 self.open_around_filled_cell(x, y)
@@ -74,6 +101,11 @@ class Field:
 
 
     def open_around_empty_cell(self, x, y):
+        """
+        Открывет все клетки вокруг пустой клетки.
+        Предварительно делается проверка, чтобы не
+        выйти за пределы поля.
+        """
         if y > 0 and not self.cells[y-1][x].is_open():
             self.open(x, y-1)
         if y > 0 and x > 0 and not self.cells[y-1][x-1].is_open():
@@ -92,6 +124,10 @@ class Field:
             self.open(x+1, y)
 
     def check_all_mines_around_are_open(self, x, y):
+        """
+        Проверяет, поставлено ли вокруг клетки столько
+        же флагов, сколько вокруг нее мин.
+        """
         mines_around = 0
         if y > 0 and self.cells[y-1][x].is_flagged():
             mines_around += 1
@@ -112,25 +148,31 @@ class Field:
         return mines_around == self.cells[y][x].content.number
     
     def open_around_filled_cell(self, x, y):
-            if y > 0 and not self.cells[y-1][x].is_open() and not self.cells[y-1][x].is_flagged():
-                self.open(x, y-1)
-            if y > 0 and x > 0 and not self.cells[y-1][x-1].is_open() and not self.cells[y-1][x-1].is_flagged():
-                self.open(x-1, y-1)
-            if y > 0 and x < self.x - 1 and not self.cells[y-1][x+1].is_open() and not self.cells[y-1][x+1].is_flagged():
-                self.open(x+1, y-1)
-            if y < self.y - 1 and not self.cells[y+1][x].is_open() and not self.cells[y+1][x].is_flagged():
-                self.open(x, y+1)
-            if y < self.y - 1 and x > 0 and not self.cells[y+1][x-1].is_open() and not self.cells[y+1][x-1].is_flagged():
-                    self.open(x-1, y+1)
-            if y < self.y - 1 and x < self.x - 1 and not self.cells[y+1][x+1].is_open() and not self.cells[y+1][x+1].is_flagged():
-                self.open(x+1, y+1)
-            if x > 0 and not self.cells[y][x-1].is_open() and not self.cells[y][x-1].is_flagged():
-                self.open(x-1, y)
-            if x < self.x - 1 and not self.cells[y][x+1].is_open() and not self.cells[y][x+1].is_flagged():
-                self.open(x+1, y)
+        """
+        Открывает все клетки вокруг данной, за исключением тех,
+        на которых стоят флаги.
+        """
+        if y > 0 and not self.cells[y-1][x].is_open() and not self.cells[y-1][x].is_flagged():
+            self.open(x, y-1)
+        if y > 0 and x > 0 and not self.cells[y-1][x-1].is_open() and not self.cells[y-1][x-1].is_flagged():
+            self.open(x-1, y-1)
+        if y > 0 and x < self.x - 1 and not self.cells[y-1][x+1].is_open() and not self.cells[y-1][x+1].is_flagged():
+            self.open(x+1, y-1)
+        if y < self.y - 1 and not self.cells[y+1][x].is_open() and not self.cells[y+1][x].is_flagged():
+            self.open(x, y+1)
+        if y < self.y - 1 and x > 0 and not self.cells[y+1][x-1].is_open() and not self.cells[y+1][x-1].is_flagged():
+                self.open(x-1, y+1)
+        if y < self.y - 1 and x < self.x - 1 and not self.cells[y+1][x+1].is_open() and not self.cells[y+1][x+1].is_flagged():
+            self.open(x+1, y+1)
+        if x > 0 and not self.cells[y][x-1].is_open() and not self.cells[y][x-1].is_flagged():
+            self.open(x-1, y)
+        if x < self.x - 1 and not self.cells[y][x+1].is_open() and not self.cells[y][x+1].is_flagged():
+            self.open(x+1, y)
             
-
     def place_flag(self, x, y):
+        """
+        Отмечает, что на клетку был поставлен флаг.
+        """
         self.cells[y][x].place_flag()
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -15,44 +15,55 @@ FIELD_WIDTH = 16
 FIELD_HEIGHT = 16
 MINES_NUMBER = 40
 field = field.Field(FIELD_WIDTH, FIELD_HEIGHT, MINES_NUMBER)
-cell_size = 35
+CELL_SIZE = 35
 
 pygame.font.init()
-font = pygame.font.SysFont('Times New Roman', 35 * cell_size // 50)
+font = pygame.font.SysFont('Times New Roman', 35 * CELL_SIZE // 50)
 info_font = pygame.font.SysFont('Arial', 20)
 
 time_passed = 0
 
 def draw_field(screen, field):
+    """
+    Отрисовывает все клетки поля. Подсчитывает, сколько клеток открыто.
+    Если открыты все клетки - вызывается функция победы в игре.
+    """
     opened = 0
     for cell_row in field.cells:
         for cell in cell_row:
             if cell.is_open():
                 opened += 1
                 if cell.content_type == "mine":
-                    pygame.draw.rect(screen, "red", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size))
+                    pygame.draw.rect(screen, "red", (cell.x*CELL_SIZE, cell.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
                     lose_game()
                 else:
-                    pygame.draw.rect(screen, (220, 220, 220), (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size))
-                    pygame.draw.rect(screen, "gray", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size), 1)
+                    pygame.draw.rect(screen, (220, 220, 220), (cell.x*CELL_SIZE, cell.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                    pygame.draw.rect(screen, "gray", (cell.x*CELL_SIZE, cell.y*CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
                     if cell.content.number != 0:
                         text = font.render(str(cell.content.number), False, cell.content.color)
-                        screen.blit(text, ((cell.x + 0.35)*cell_size, (cell.y + 0.1)*cell_size))
+                        screen.blit(text, ((cell.x + 0.35)*CELL_SIZE, (cell.y + 0.1)*CELL_SIZE))
             elif cell.is_flagged():
-                pygame.draw.polygon(screen, "red", [((cell.x + 0.2)*cell_size, (cell.y + 0.5)*cell_size),
-                                                    ((cell.x + 0.8)*cell_size, (cell.y + 0.2)*cell_size),
-                                                    ((cell.x + 0.8)*cell_size, (cell.y + 0.8)*cell_size)])
-                pygame.draw.rect(screen, "black", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size), 3)
+                pygame.draw.polygon(screen, "red", [((cell.x + 0.2)*CELL_SIZE, (cell.y + 0.5)*CELL_SIZE),
+                                                    ((cell.x + 0.8)*CELL_SIZE, (cell.y + 0.2)*CELL_SIZE),
+                                                    ((cell.x + 0.8)*CELL_SIZE, (cell.y + 0.8)*CELL_SIZE)])
+                pygame.draw.rect(screen, "black", (cell.x*CELL_SIZE, cell.y*CELL_SIZE, CELL_SIZE, CELL_SIZE), 3)
             else:
-                pygame.draw.rect(screen, "gray", (cell.x*cell_size, cell.y*cell_size, cell_size, cell_size), 1)
+                pygame.draw.rect(screen, "gray", (cell.x*CELL_SIZE, cell.y*CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
     if opened == FIELD_WIDTH*FIELD_HEIGHT - MINES_NUMBER:
         win_game()
 
 def draw_timer(time_passed):
+    """
+    Отрисовывает таймер, показывающий, сколько прошло времени.
+    """
     text = info_font.render("Time: " + str(time_passed//1000), False, "black")
     screen.blit(text, (WIDTH - 120, 20))
 
 def win_game():
+    """
+    Вызывается при победе в игре. Проверяет на новый рекорд,
+    выводит прошедшее время в консоль.
+    """
     global finished
     finished = True
     old_record = 0
@@ -65,6 +76,10 @@ def win_game():
             f.write(str(time))
 
 def lose_game():
+    """
+    Вызывается, если игра проиграна.
+    Выводит соответсвующее сообщение в консоль.
+    """
     global finished
     finished = True
     print("You loser")
@@ -85,8 +100,8 @@ while not finished:
             if event.button == 1:           # Left mouse button
                 if firstClick:
                     firstClick = False
-                    field.create_island(event.pos[0] // cell_size, event.pos[1] // cell_size)
-                field.open(event.pos[0] // cell_size, event.pos[1] // cell_size)
+                    field.create_island(event.pos[0] // CELL_SIZE, event.pos[1] // CELL_SIZE)
+                field.open(event.pos[0] // CELL_SIZE, event.pos[1] // CELL_SIZE)
             elif event.button == 3:         # Right mouse button
-                field.place_flag(event.pos[0] // cell_size, event.pos[1] // cell_size)
+                field.place_flag(event.pos[0] // CELL_SIZE, event.pos[1] // CELL_SIZE)
     pygame.display.update()
